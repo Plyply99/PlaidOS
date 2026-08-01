d97e914474aaa0b5ec76da9c750a5bfe2e5f96b6
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2026-08-01 13:18:32 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,27 +12,27 @@ dnf5 -y swap mesa-va-drivers mesa-va-drivers-freeworld --allowerasing --enablere
 dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
 
 # Add COPR and TERRA repos
-dnf5 -y copr enable avengemedia/dms-git 
-dnf5 -y copr enable yalter/niri-git
-dnf5 -y copr enable lionheartp/Hyprland #nett00n/hyprland
+#dnf5 -y copr enable avengemedia/dms-git 
+#dnf5 -y copr enable yalter/niri-git
+#dnf5 -y copr enable lionheartp/Hyprland #nett00n/hyprland
 dnf5 -y copr enable ublue-os/akmods 
 dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 
 # Use niri-git instead of fedora repo
-echo "priority=1" | tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo
+#echo "priority=1" | tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo
 
 ### Install packages
 dnf5 -y install mutter-devel gobject-introspection-devel glib2-devel meson ninja-build gcc-c++
-dnf5 -y install steam-devices
-dnf5 -y install plymouth plymouth-theme-spinner ly rpmdevtools akmods audit pciutils nethogs iotop
-dnf5 -y install alsa-firmware cups-pk-helper fprintd fprintd-pam i2c-tools kf6-kimageformats khal power-profiles-daemon gnome-software gnome-software-rpm-ostree htop xwininfo glibc-locale-source glibc-langpack-en libva-utils
-dnf5 -y install cups cups-filters system-config-printer gutenprint gutenprint-cups bluez-cups NetworkManager-wifi avahi-dnsconfd firewalld firewall-offline-cmd distrobox smartmontools speech-dispatcher
-dnf5 -y install adw-gtk3-theme bat bat-extras btop chafa cliphist dgop dms dsearch emacs eza fastfetch gamemode ghostty gnome-disk-utility grim input-remapper mangohud mpv nautilus niri npm nwg-look python3-dbus-next qt6-qtmultimedia slurp vkBasalt
-dnf5 -y install hyprland hyprland-guiutils hyprpicker #--allowerasing --skip-broken
-dnf5 -y install gnome-shell
+dnf5 -y install steam-devices distrobox mpv input-remapper mangohud vkBasalt adw-gtk3-theme bat bat-extras btop chafa emacs eza fastfetch gamemode ghostty htop xwininfo libva-utils 
+#dnf5 -y install plymouth plymouth-theme-spinner ly rpmdevtools akmods audit pciutils nethogs iotop
+#dnf5 -y install alsa-firmware cups-pk-helper fprintd fprintd-pam i2c-tools kf6-kimageformats khal power-profiles-daemon gnome-software gnome-software-rpm-ostree htop xwininfo glibc-locale-source glibc-langpack-en libva-utils
+#dnf5 -y install cups cups-filters system-config-printer gutenprint gutenprint-cups bluez-cups NetworkManager-wifi avahi-dnsconfd firewalld firewall-offline-cmd distrobox smartmontools speech-dispatcher
+#dnf5 -y install adw-gtk3-theme bat bat-extras btop chafa cliphist dgop dms dsearch emacs eza fastfetch gamemode ghostty gnome-disk-utility grim input-remapper mangohud mpv nautilus niri npm nwg-look python3-dbus-next qt6-qtmultimedia slurp vkBasalt
+#dnf5 -y install hyprland hyprland-guiutils hyprpicker #--allowerasing --skip-broken
+#dnf5 -y install gnome-shell
 
 # Remove niri-git and hyprland optional dependencies
-dnf5 -y remove alacritty fuzzel kitty mako swaybg swayidle swaylock SwayNotificationCenter waybar wofi
+#dnf5 -y remove alacritty fuzzel kitty mako swaybg swayidle swaylock SwayNotificationCenter waybar wofi
 dnf5 clean all
 
 # Ly login manager
@@ -42,19 +42,19 @@ dnf5 clean all
 
 #### Example for enabling a System Unit File
 #systemctl disable gdm  ly@tty2.service # Enabled automatically when installing gnome
-systemctl enable avahi-daemon.service firewalld.service NetworkManager.service rtkit-daemon.service plymouth-start.service
-systemctl enable cups.socket
+#systemctl enable avahi-daemon.service firewalld.service NetworkManager.service rtkit-daemon.service plymouth-start.service
+#systemctl enable cups.socket
 systemctl mask bootc-fetch-apply-updates.timer #turn off update timer
 
 # Plymouth prettiness
-systemctl enable plymouth-start.service
-set -x; \
-    kver=$(cd /usr/lib/modules && echo *); \
-    dracut -vf --no-machineid /usr/lib/modules/$kver/initramfs.img $kver
+#systemctl enable plymouth-start.service
+#set -x; \
+#    kver=$(cd /usr/lib/modules && echo *); \
+#    dracut -vf --no-machineid /usr/lib/modules/$kver/initramfs.img $kver
 
 #Set locale and environment
-localedef -i en_US -f UTF-8 en_US.UTF-8
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+#localedef -i en_US -f UTF-8 en_US.UTF-8
+#echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 # Set os-release
 HOME_URL="https://github.com/Plyply99/PlyplyOS"
