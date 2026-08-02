6ce5553d9f1156930e32552a51be0b669ba2f3a4
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2026-08-02 08:00:28 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -24,14 +24,14 @@ dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/ter
 #echo "priority=1" | tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo
 
 ### Install packages
-dnf5 -y install mutter-devel gobject-introspection-devel glib2-devel meson ninja-build gcc-c++ gstreamer1-vaapi gstreamer1-libav
-dnf5 -y install gdm akmods steam-devices distrobox mpv input-remapper mangohud vkBasalt adw-gtk3-theme bat bat-extras btop chafa emacs eza fastfetch gamemode ghostty htop xwininfo libva-utils 
+dnf5 -y install mutter-devel gobject-introspection-devel glib2-devel meson ninja-build gcc-c++ gstreamer1-vaapi gstreamer1-libav #Plaid stuff
+dnf5 -y install gdm akmods steam-devices distrobox mpv input-remapper mangohud vkBasalt adw-gtk3-theme bat bat-extras btop cava chafa emacs eza fastfetch gamemode ghostty htop xwininfo libva-utils 
+dnf5 -y install nautilus gnome-software gnome-software-rpm-ostree
 #dnf5 -y install plymouth plymouth-theme-spinner ly rpmdevtools akmods audit pciutils nethogs iotop
 #dnf5 -y install alsa-firmware cups-pk-helper fprintd fprintd-pam i2c-tools kf6-kimageformats khal power-profiles-daemon gnome-software gnome-software-rpm-ostree htop xwininfo glibc-locale-source glibc-langpack-en libva-utils
 #dnf5 -y install cups cups-filters system-config-printer gutenprint gutenprint-cups bluez-cups NetworkManager-wifi avahi-dnsconfd firewalld firewall-offline-cmd distrobox smartmontools speech-dispatcher
 #dnf5 -y install adw-gtk3-theme bat bat-extras btop chafa cliphist dgop dms dsearch emacs eza fastfetch gamemode ghostty gnome-disk-utility grim input-remapper mangohud mpv nautilus niri npm nwg-look python3-dbus-next qt6-qtmultimedia slurp vkBasalt
 #dnf5 -y install hyprland hyprland-guiutils hyprpicker #--allowerasing --skip-broken
-dnf5 -y install gnome-shell nautilus gnome-software gnome-software-rpm-ostree
 
 # Remove niri-git and hyprland optional dependencies
 #dnf5 -y remove alacritty fuzzel kitty mako swaybg swayidle swaylock SwayNotificationCenter waybar wofi
