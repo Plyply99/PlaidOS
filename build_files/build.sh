a7e3bca8b846d7a9c86045e4b6473a089b02a5da
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2026-08-05 12:16:55 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,9 +14,9 @@ dnf5 -y install libavcodec-freeworld
 dnf5 -y --setopt=obsoletes=0 install gstreamer1-vaapi
 
 # Add COPR and TERRA repos
-#dnf5 -y copr enable avengemedia/dms-git 
-#dnf5 -y copr enable yalter/niri-git
-#dnf5 -y copr enable lionheartp/Hyprland #nett00n/hyprland
+dnf5 -y copr enable avengemedia/dms-git 
+dnf5 -y copr enable yalter/niri-git
+dnf5 -y copr enable lionheartp/Hyprland #nett00n/hyprland
 dnf5 -y copr enable ublue-os/akmods 
 dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 
@@ -24,17 +24,17 @@ dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/ter
 #echo "priority=1" | tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo
 
 ### Install packages
+#dnf5 -y install gdm akmods steam-devices distrobox mpv input-remapper mangohud vkBasalt adw-gtk3-theme bat bat-extras btop cava chafa emacs eza fastfetch gamemode ghostty htop xwininfo libva-utils rpmdevtools kitty nethogs iotop amdgpu_top
 dnf5 -y install mutter-devel gobject-introspection-devel glib2-devel meson ninja-build gcc-c++ gstreamer1-vaapi gstreamer1-libav #Plaid stuff
-dnf5 -y install gdm akmods steam-devices distrobox mpv input-remapper mangohud vkBasalt adw-gtk3-theme bat bat-extras btop cava chafa emacs eza fastfetch gamemode ghostty htop xwininfo libva-utils rpmdevtools kitty nethogs iotop amdgpu_top
-dnf5 -y install nautilus gnome-software gnome-software-rpm-ostree
-#dnf5 -y install plymouth plymouth-theme-spinner ly rpmdevtools akmods audit pciutils nethogs iotop
-#dnf5 -y install alsa-firmware cups-pk-helper fprintd fprintd-pam i2c-tools kf6-kimageformats khal power-profiles-daemon gnome-software gnome-software-rpm-ostree htop xwininfo glibc-locale-source glibc-langpack-en libva-utils
-#dnf5 -y install cups cups-filters system-config-printer gutenprint gutenprint-cups bluez-cups NetworkManager-wifi avahi-dnsconfd firewalld firewall-offline-cmd distrobox smartmontools speech-dispatcher
-#dnf5 -y install adw-gtk3-theme bat bat-extras btop chafa cliphist dgop dms dsearch emacs eza fastfetch gamemode ghostty gnome-disk-utility grim input-remapper mangohud mpv nautilus niri npm nwg-look python3-dbus-next qt6-qtmultimedia slurp vkBasalt
-#dnf5 -y install hyprland hyprland-guiutils hyprpicker #--allowerasing --skip-broken
+dnf5 -y install nautilus gnome-software gnome-software-rpm-ostree steam-devices
+dnf5 -y install plymouth plymouth-theme-spinner ly rpmdevtools akmods audit pciutils nethogs iotop
+dnf5 -y install alsa-firmware cups-pk-helper fprintd fprintd-pam i2c-tools kf6-kimageformats khal power-profiles-daemon gnome-software gnome-software-rpm-ostree htop xwininfo glibc-locale-source glibc-langpack-en libva-utils
+dnf5 -y install cups cups-filters system-config-printer gdm gutenprint gutenprint-cups bluez-cups NetworkManager-wifi avahi-dnsconfd firewalld firewall-offline-cmd distrobox smartmontools speech-dispatcher amdgpu_top
+dnf5 -y install adw-gtk3-theme bat bat-extras btop chafa cliphist dgop dms dsearch emacs eza fastfetch gamemode ghostty gnome-disk-utility grim input-remapper mangohud mpv nautilus niri npm nwg-look python3-dbus-next qt6-qtmultimedia slurp vkBasalt
+dnf5 -y install hyprland hyprland-guiutils hyprpicker #--allowerasing --skip-broken
 
 # Remove niri-git and hyprland optional dependencies
-#dnf5 -y remove alacritty fuzzel kitty mako swaybg swayidle swaylock SwayNotificationCenter waybar wofi
+dnf5 -y remove fuzzel mako swaybg swayidle swaylock SwayNotificationCenter waybar wofi
 dnf5 clean all
 
 # Ly login manager
@@ -43,32 +43,32 @@ dnf5 clean all
 #restorecon -v /usr/bin/ly
 
 #### Example for enabling a System Unit File
-#systemctl disable gdm  ly@tty2.service # Enabled automatically when installing gnome
-#systemctl enable avahi-daemon.service firewalld.service NetworkManager.service rtkit-daemon.service plymouth-start.service
-#systemctl enable cups.socket
+#systemctl disable gdm # Enabled automatically when installing gnome # ly@tty2.service
+systemctl enable avahi-daemon.service firewalld.service NetworkManager.service rtkit-daemon.service plymouth-start.service
+systemctl enable cups.socket
 systemctl mask bootc-fetch-apply-updates.timer #turn off update timer
 
 # Plymouth prettiness
-#systemctl enable plymouth-start.service
-#set -x; \
-#    kver=$(cd /usr/lib/modules && echo *); \
-#    dracut -vf --no-machineid /usr/lib/modules/$kver/initramfs.img $kver
+systemctl enable plymouth-start.service
+set -x; \
+    kver=$(cd /usr/lib/modules && echo *); \
+    dracut -vf --no-machineid /usr/lib/modules/$kver/initramfs.img $kver
 
 #Set locale and environment
-#localedef -i en_US -f UTF-8 en_US.UTF-8
-#echo "LANG=en_US.UTF-8" > /etc/locale.conf
+localedef -i en_US -f UTF-8 en_US.UTF-8
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 # Set os-release
-HOME_URL="https://github.com/Plyply99/PlyplyOS"
+HOME_URL="https://github.com/Plyply99/PlaidOS"
 sed -i -f - /usr/lib/os-release <<EOF
-s|^NAME=.*|NAME=\"PlyplyOS\"|
-s|^PRETTY_NAME=.*|PRETTY_NAME=\"PlyplyOS built $(date +"%y/%m/%d")\"|
+s|^NAME=.*|NAME=\"PlaidOS\"|
+s|^PRETTY_NAME=.*|PRETTY_NAME=\"PlaidOS built $(date +"%y/%m/%d")\"|
 s|^VERSION_CODENAME=.*|VERSION_CODENAME=\"44\"|
 s|^VARIANT_ID=.*|VARIANT_ID=""|
 s|^HOME_URL=.*|HOME_URL=\"${HOME_URL}\"|
 s|^BUG_REPORT_URL=.*|BUG_REPORT_URL=\"${HOME_URL}/issues\"|
 s|^SUPPORT_URL=.*|SUPPORT_URL=\"${HOME_URL}/issues\"|
-s|^CPE_NAME=\".*\"|CPE_NAME=\"cpe:/o:plyplyos-dev:plyplyos\"|
+s|^CPE_NAME=\".*\"|CPE_NAME=\"cpe:/o:plaidos-dev:plaidos\"|
 s|^DOCUMENTATION_URL=.*|DOCUMENTATION_URL=\"${HOME_URL}\"|
 #s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME="plyply-pc"|
 
