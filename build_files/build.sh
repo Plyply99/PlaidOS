cbe2dfd44f1b2d8242e054c6473f7ea170c4e28e
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2026-08-06 09:08:38 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,39 +2,12 @@
 
 set -ouex pipefail
 
-# Add dnf plugins
-dnf5 -y install dnf5-plugins
-
-# Add hardware codecs and multimedia
-dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf5 -y install @multimedia
-dnf5 -y swap mesa-va-drivers mesa-va-drivers-freeworld --allowerasing --enablerepo=rpmfusion-free-updates-testing
-dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
-dnf5 -y install libavcodec-freeworld
-dnf5 -y --setopt=obsoletes=0 install gstreamer1-vaapi
-
-# Add COPR and TERRA repos
-dnf5 -y copr enable avengemedia/dms-git 
-dnf5 -y copr enable yalter/niri-git
-dnf5 -y copr enable lionheartp/Hyprland #nett00n/hyprland
+# Additional repos
 dnf5 -y copr enable ublue-os/akmods 
 dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 
-# Use niri-git instead of fedora repo
-#echo "priority=1" | tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo
-
 ### Install packages
-#dnf5 -y install gdm akmods steam-devices distrobox mpv input-remapper mangohud vkBasalt adw-gtk3-theme bat bat-extras btop cava chafa emacs eza fastfetch gamemode ghostty htop xwininfo libva-utils rpmdevtools kitty nethogs iotop amdgpu_top
-dnf5 -y install mutter-devel gobject-introspection-devel glib2-devel meson ninja-build gcc-c++ gstreamer1-vaapi gstreamer1-libav #Plaid stuff
-dnf5 -y install nautilus gnome-software gnome-software-rpm-ostree steam-devices
-dnf5 -y install plymouth plymouth-theme-spinner ly rpmdevtools akmods audit pciutils nethogs iotop
-dnf5 -y install alsa-firmware cups-pk-helper fprintd fprintd-pam i2c-tools kf6-kimageformats khal power-profiles-daemon gnome-software gnome-software-rpm-ostree htop xwininfo glibc-locale-source glibc-langpack-en libva-utils
-dnf5 -y install cups cups-filters system-config-printer gdm gutenprint gutenprint-cups bluez-cups NetworkManager-wifi avahi-dnsconfd firewalld firewall-offline-cmd distrobox smartmontools speech-dispatcher amdgpu_top
-dnf5 -y install adw-gtk3-theme bat bat-extras btop chafa cliphist dgop dms dsearch emacs eza fastfetch gamemode ghostty gnome-disk-utility grim input-remapper mangohud mpv nautilus niri npm nwg-look python3-dbus-next qt6-qtmultimedia slurp vkBasalt
-dnf5 -y install hyprland hyprland-guiutils hyprpicker #--allowerasing --skip-broken
-
-# Remove niri-git and hyprland optional dependencies
-dnf5 -y remove fuzzel mako swaybg swayidle swaylock SwayNotificationCenter waybar wofi
+dnf5 -y install bat bat-extras btop cava chafa emacs fastfetch htop input-remapper mangohud mpv nodejs24 vkBasalt
 dnf5 clean all
 
 # Ly login manager
@@ -44,25 +17,12 @@ dnf5 clean all
 
 #### Example for enabling a System Unit File
 #systemctl disable gdm # Enabled automatically when installing gnome # ly@tty2.service
-systemctl enable avahi-daemon.service firewalld.service NetworkManager.service rtkit-daemon.service plymouth-start.service
-systemctl enable cups.socket
-systemctl mask bootc-fetch-apply-updates.timer #turn off update timer
-
-# Plymouth prettiness
-systemctl enable plymouth-start.service
-set -x; \
-    kver=$(cd /usr/lib/modules && echo *); \
-    dracut -vf --no-machineid /usr/lib/modules/$kver/initramfs.img $kver
-
-#Set locale and environment
-localedef -i en_US -f UTF-8 en_US.UTF-8
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 # Set os-release
 HOME_URL="https://github.com/Plyply99/PlaidOS"
 sed -i -f - /usr/lib/os-release <<EOF
 s|^NAME=.*|NAME=\"PlaidOS\"|
-s|^PRETTY_NAME=.*|PRETTY_NAME=\"PlaidOS built $(date +"%y/%m/%d")\"|
+s|^PRETTY_NAME=.*|PRETTY_NAME=\"PlaidOS built $(date +"%y-%m-%d")\"|
 s|^VERSION_CODENAME=.*|VERSION_CODENAME=\"44\"|
 s|^VARIANT_ID=.*|VARIANT_ID=""|
 s|^HOME_URL=.*|HOME_URL=\"${HOME_URL}\"|
